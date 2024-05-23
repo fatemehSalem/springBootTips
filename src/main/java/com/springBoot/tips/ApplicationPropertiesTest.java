e84472f8edf9e1600c7be696b34d67506eaaa64f
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-23 06:22:21 +0330

    configuration via configurationProperties

--- src/main/java/com/springBoot/tips/ApplicationPropertiesTest.java
+++ src/main/java/com/springBoot/tips/ApplicationPropertiesTest.java
@@ -3,30 +3,29 @@ package com.springBoot.tips;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "app")
 @Validated
 public class ApplicationPropertiesTest {
 
-    private String version;
+    private final String version;
 
-    private FtpProperties ftp;
+    private final FtpProperties ftp;
+
+    @ConstructorBinding
+    public ApplicationPropertiesTest(String version, FtpProperties ftp) {
+        this.version = version;
+        this.ftp = ftp;
+    }
 
     public FtpProperties getFtp() {
         return ftp;
     }
 
-    public void setFtp(FtpProperties ftp) {
-        this.ftp = ftp;
-    }
-
     public String getVersion() {
         return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
     }
 
     @Override
@@ -39,44 +38,36 @@ public class ApplicationPropertiesTest {
 
     private static class FtpProperties{
         @NotEmpty
-        private String host;
+        private final String host;
         @Positive
-        private int port;
+        private final int port;
         @NotEmpty
-        private String userName;
+        private final String userName;
         @NotEmpty
-        private String password;
+        private final String password;
+
+        @ConstructorBinding
+        private FtpProperties(String host, int port, String userName, String password) {
+            this.host = host;
+            this.port = port;
+            this.userName = userName;
+            this.password = password;
+        }
 
         public String getHost() {
             return host;
-        }
-
-        public void setHost(String host) {
-            this.host = host;
         }
 
         public int getPort() {
             return port;
         }
 
-        public void setPort(int port) {
-            this.port = port;
-        }
-
         public String getUserName() {
             return userName;
         }
 
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
         public String getPassword() {
             return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
         }
 
         @Override

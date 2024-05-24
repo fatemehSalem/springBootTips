2dc35ccdf50d0d832fd42641251870ed175cba00
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-24 08:21:10 +0330

    Unit test for ProductController

--- src/test/java/com/springBoot/tips/rest/ProductControllerTest.java
+++ src/test/java/com/springBoot/tips/rest/ProductControllerTest.java
@@ -1,16 +1,21 @@
 package com.springBoot.tips.rest;
 
+import com.springBoot.tips.domain.Product;
 import com.springBoot.tips.domain.ProductService;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
@@ -24,8 +29,23 @@ class ProductControllerTest {
 
     @Test
     void shouldReturnProducts() throws Exception {
-        BDDMockito.given(productService.getAllProducts()).willReturn(List.of());
+        Product p1 = new Product(1L , "name1" , "desc1" , BigDecimal.TEN , true);
+        Product p2 = new Product(2L , "name2" , "desc2" , BigDecimal.TEN , false);
+        BDDMockito.given(productService.getAllProducts()).willReturn(List.of(p1 , p2));
 
-        mockMvc.perform(get("/api/products")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].name").value("name1"))
+                .andExpect(jsonPath("$[0].description").value("desc1"))
+                .andExpect(jsonPath("$[0].price").value(10))
+                .andExpect(jsonPath("$[0].disabled").value(true))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].name").value("name2"))
+                .andExpect(jsonPath("$[1].description").value("desc2"))
+                .andExpect(jsonPath("$[1].price").value(10))
+                .andExpect(jsonPath("$[1].disabled").value(false));
     }
 }

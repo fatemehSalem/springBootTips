af3fca3756a50750b3e08aca5512ec52370981b2
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-26 09:57:02 +0330

    Exception Handling thrown by Global Exception Handling

--- src/main/java/com/springBoot/tips/rest/CustomerController.java
+++ src/main/java/com/springBoot/tips/rest/CustomerController.java
@@ -1,15 +1,11 @@
 package com.springBoot.tips.rest;
 
 import com.springBoot.tips.domain.Customer;
-import com.springBoot.tips.domain.CustomerNotFoundException;
 import com.springBoot.tips.domain.CustomerService;
-import com.springBoot.tips.domain.ErrorResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -28,11 +24,5 @@ public class CustomerController {
     public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
         Customer customer = customerService.getCustomerById(id);
         return ResponseEntity.ok(customer);
-    }
-
-    @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(CustomerNotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }

16eae21103b27a8d332af19b8e6b0a7bbe5666c5
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-27 08:45:30 +0330

    Exception Handling thrown by Problem springWeb

--- src/main/java/com/springBoot/tips/domain/Customer.java
+++ src/main/java/com/springBoot/tips/domain/Customer.java
@@ -1,6 +1,8 @@
 package com.springBoot.tips.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,9 +23,12 @@ public class Customer {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @NotEmpty(message = "Email can not be Empty")
+    @Email(message = "Email is invalid")
     private String email;
 
     @Column(nullable = false)
+    @NotEmpty(message = "Name can not be Empty")
     private String name;
 
 }
--- src/main/java/com/springBoot/tips/rest/CustomerController.java
+++ src/main/java/com/springBoot/tips/rest/CustomerController.java
@@ -3,8 +3,10 @@ package com.springBoot.tips.rest;
 import com.springBoot.tips.domain.Customer;
 import com.springBoot.tips.domain.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.List;
 
@@ -24,5 +26,11 @@ public class CustomerController {
     public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
         Customer customer = customerService.getCustomerById(id);
         return ResponseEntity.ok(customer);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody @Valid Customer customer) {
+        return customerService.createCustomer(customer);
     }
 }

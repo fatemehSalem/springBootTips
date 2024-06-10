7427d174909bc76f3e8a037c9b879bb784ab1809
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-06-10 13:32:36 +0330

    embedded entities

---
+++ src/main/java/com/springBoot/tips/models/embedded/Address.java
@@ -0,0 +1,20 @@
+package com.springBoot.tips.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+}
---
+++ src/main/java/com/springBoot/tips/models/embedded/Person.java
@@ -0,0 +1,30 @@
+package com.springBoot.tips.models.embedded;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
+            @AttributeOverride(name = "state", column = @Column(name = "home_state")),
+            @AttributeOverride(name = "zipCode", column = @Column(name = "home_zip_code"))
+    })
+    private Address address;
+}
